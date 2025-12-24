9b59d065e1e2071e6655529a0924d9b95356dc9f
CoachLT11 <loganturner1125@outlook.com>
2025-12-24 12:46:37 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE = "clone-armor-trainer-v1";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./sw.js"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
